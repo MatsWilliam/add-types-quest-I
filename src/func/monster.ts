47c0e4e6fa6f62dce0053e05c41681f4b213d330
MatsWilliam <mats.blomqvist@hotmail.com>
2024-12-11 14:01:22 +0100

    Första etappen med monster

--- src/func/monster.ts
+++ src/func/monster.ts
@@ -1,19 +1,40 @@
+import {Monster} from "./types";
+
+
 //utöka så alla egenskaper för monstret skrivs ut
 
-export const prettyPrintMonster = (m) => {
-	console.log(`Monster: ${m.name}`);
+export const prettyPrintMonster = (allMonsters:Monster[]) => {
+
+	allMonsters.forEach((m) => {
+		console.log(`Name: ${m.name}, Age: ${m.age}, Antal tentakler: ${m.tentacles}, Antal ögon: ${m.eyes}, Finns vingar? ${m.hasWings}`);
+	})
 };
 
-export const averageMonsterAge = (m) => {
-	return 2;
+export const averageMonsterAge = (monsters:Monster[]) => {
+	let allAges: number = 0;
+	monsters.forEach((m) => {
+		allAges += m.age;
+	});
+	
+	return Math.round (allAges / monsters.length);
 };
 
-export const averageNumberOfTentacles = (m) => {
-	return 2;
+export const averageNumberOfTentacles = (monsters:Monster[]) => {
+	let allTentacles: number = 0;
+	monsters.forEach((m) => {
+		allTentacles += m.tentacles;
+	})
+	return Math.round (allTentacles / monsters.length);
 };
 
-export const numberOfMonstersWithWings = (m) => {
-	return 0;
+export const numberOfMonstersWithWings = (monsters:Monster[]) => {
+	let monstersWithWings: number = 0;
+	monsters.forEach((m) => {
+		if (m.hasWings===true) {
+			monstersWithWings += 1			
+		}
+	})
+	return monstersWithWings;
 };
 
 export const getAllNoWingedMonster = (m) => {
